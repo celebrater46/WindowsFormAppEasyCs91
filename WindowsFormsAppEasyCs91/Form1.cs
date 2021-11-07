18c7961afaaca42f7727792f46c0b751a603cf2f
hideru <jota.16.stigma.world@gmail.com>
2021-11-08 00:59:50 +0900

    helloworld test succeeded

--- WindowsFormsAppEasyCs91/Form1.cs
+++ WindowsFormsAppEasyCs91/Form1.cs
@@ -12,15 +12,17 @@ namespace WindowsFormsAppEasyCs91
 {
     public partial class Form1 : Form
     {
-        private TableLayoutPanel tlp;
-        private RadioButton[] rb = new RadioButton[4];
-        private Image cim;
-        private Image[] mim = new Image[4];
-        private PictureBox pb;
+        // private TableLayoutPanel tlp;
+        // private RadioButton[] rb = new RadioButton[4];
+        // private Image cim;
+        // private Image[] mim = new Image[4];
+        // private PictureBox pb;
+        // private Label lb;
+        // private int num;
+        // private bool isOpen; // Is the card open?
+        // private string picturePass = "C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs91\\WindowsFormsAppEasyCs91\\img\\";
+        
         private Label lb;
-        private int num;
-        private bool isOpen; // Is the card open?
-        private string picturePass = "C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs91\\WindowsFormsAppEasyCs91\\img\\";
         
         public Form1()
         {
@@ -29,72 +31,80 @@ namespace WindowsFormsAppEasyCs91
             this.Width = 650;
             this.Height = 450;
 
-            tlp = new TableLayoutPanel();
-            tlp.Dock = DockStyle.Fill;
-            tlp.ColumnCount = 4;
-            tlp.RowCount = 2;
-
-            for (int i = 0; i < rb.Length; i++)
-            {
-                mim[i] = Image.FromFile(picturePass + "mark" + i + ".bmp");
-                rb[i] = new RadioButton();
-                rb[i].Image = mim[i];
-                rb[i].AutoSize = true;
-                rb[i].Parent = tlp;
-            }
-
-            cim = Image.FromFile(picturePass + "card.bmp");
-            pb = new PictureBox();
-            pb.Image = cim;
-            pb.SizeMode = PictureBoxSizeMode.AutoSize;
-            pb.Anchor = AnchorStyles.Right;
-            pb.Parent = tlp;
-
+            // For Test
             lb = new Label();
-            lb.Font = new Font("SansSefif", 50, FontStyle.Bold);
-            lb.AutoSize = true;
-            lb.Anchor = AnchorStyles.None;
-            lb.Parent = tlp;
-            
-            tlp.SetColumnSpan(pb, 2);
-            tlp.SetColumnSpan(lb, 2);
+            lb.Text = "HelloWorld";
+            lb.Width = 250;
+            lb.Parent = this;
 
-            tlp.Parent = this;
-
-            isOpen = false;
-            Random rn = new Random();
-            num = rn.Next(4);
-
-            pb.Click += new EventHandler(PbClick);
+            // tlp = new TableLayoutPanel();
+            // tlp.Dock = DockStyle.Fill;
+            // tlp.ColumnCount = 4;
+            // tlp.RowCount = 2;
+            //
+            // for (int i = 0; i < rb.Length; i++)
+            // {
+            //     // mim[i] = Image.FromFile(picturePass + "mark" + i + ".bmp");
+            //     mim[i] = Image.FromFile(string.Format("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs91\\WindowsFormsAppEasyCs91\\img\\mark{0}.bmp", i));
+            //     rb[i] = new RadioButton();
+            //     rb[i].Image = mim[i];
+            //     rb[i].AutoSize = true;
+            //     rb[i].Parent = tlp;
+            // }
+            //
+            // // cim = Image.FromFile(picturePass + "card.bmp");
+            // cim = Image.FromFile("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs91\\WindowsFormsAppEasyCs91\\img\\card.bmp");
+            // pb = new PictureBox();
+            // pb.Image = cim;
+            // pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            // pb.Anchor = AnchorStyles.Right;
+            // pb.Parent = tlp;
+            //
+            // lb = new Label();
+            // lb.Font = new Font("SansSefif", 50, FontStyle.Bold);
+            // lb.AutoSize = true;
+            // lb.Anchor = AnchorStyles.None;
+            // lb.Parent = tlp;
+            //
+            // tlp.SetColumnSpan(pb, 2);
+            // tlp.SetColumnSpan(lb, 2);
+            //
+            // tlp.Parent = this;
+            //
+            // isOpen = false;
+            // Random rn = new Random();
+            // num = rn.Next(4);
+            //
+            // pb.Click += new EventHandler(PbClick);
         }
 
-        public void PbClick(Object sender, EventArgs e)
-        {
-            if (!isOpen)
-            {
-                isOpen = true;
-                pb.Image = mim[num];
-
-                if (rb[num].Checked)
-                {
-                    lb.ForeColor = Color.DeepPink;
-                    lb.Text = "HIT!!";
-                }
-                else
-                {
-                    lb.ForeColor = Color.SlateBlue;
-                    lb.Text = "MISS!!";
-                }
-            }
-            else
-            {
-                isOpen = false;
-                lb.Text = "";
-                pb.Image = cim;
-
-                Random rn = new Random();
-                num = rn.Next(4);
-            }
-        }
+        // public void PbClick(Object sender, EventArgs e)
+        // {
+        //     if (!isOpen)
+        //     {
+        //         isOpen = true;
+        //         pb.Image = mim[num];
+        //
+        //         if (rb[num].Checked)
+        //         {
+        //             lb.ForeColor = Color.DeepPink;
+        //             lb.Text = "HIT!!";
+        //         }
+        //         else
+        //         {
+        //             lb.ForeColor = Color.SlateBlue;
+        //             lb.Text = "MISS!!";
+        //         }
+        //     }
+        //     else
+        //     {
+        //         isOpen = false;
+        //         lb.Text = "";
+        //         pb.Image = cim;
+        //
+        //         Random rn = new Random();
+        //         num = rn.Next(4);
+        //     }
+        // }
     }
 }
